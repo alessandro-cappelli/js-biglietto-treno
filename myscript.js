fe69234abd2d74198de17a59e8f872eafc46cc3c
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-04 15:56:02 +0100

    js fatto

--- myscript.js
+++ myscript.js
@@ -1,4 +1,15 @@
 let kilometer = parseInt (prompt ('Quanti km deve percorrere?'));
 let myOld = parseInt (prompt ('inserisca la sua età'));
 let priceKm = kilometer * 0.21;
+console.log(priceKm)
+
+if(myOld < 18){
+    let priceUnderage = priceKm - (priceKm * 20 / 100);
+    console.log(priceUnderage)
+}
+
+else if(myOld > 65){
+    let priceSenior = priceKm - (priceKm * 40 / 100);
+    console.log (priceSenior)
+}
 
