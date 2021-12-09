6ab65613f0b5d426aacfe86c6c59e3564d95196b
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-09 10:09:28 +0100

    aggiunto else

--- myscript.js
+++ myscript.js
@@ -1,15 +1,23 @@
 let kilometer = parseInt (prompt ('Quanti km deve percorrere?'));
 let myOld = parseInt (prompt ('inserisca la sua età'));
 let priceKm = kilometer * 0.21;
+
 console.log(priceKm)
 
 if(myOld < 18){
-    let priceUnderage = priceKm - (priceKm * 20 / 100);
-    console.log(priceUnderage)
+    priceUnderage = priceKm - (priceKm * 20 / 100);
+    messaggio = "Prezzo biglietto con sconto del 20%: "+ priceUnderage.toFixed(2) +" Euro";
+    console.log(priceUnderage);
 }
 
 else if(myOld > 65){
-    let priceSenior = priceKm - (priceKm * 40 / 100);
-    console.log (priceSenior)
+    priceSenior = priceKm - (priceKm * 40 / 100);
+    messaggio = "Prezzo biglietto con sconto del 40% : "+ priceSenior.toFixed(2) +" Euro";
+    console.log (priceSenior);
 }
 
+else{
+    messaggio = "Prezzo biglietto: "+priceKm+" Euro";
+}
+
+document.writeln(messaggio);
